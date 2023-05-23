8f4fe0f1f73d06a19955d84cab0fbc2a722baa8a
Bruce <bruce.hillsworth@gmail.com>
2023-05-22 19:26:00 -0600

    Pass tests

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,21 @@
-class MySet:
+import ipdb
 
-    pass
+
+class MySet:
+    def __init__(self, enumerable=[]):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
